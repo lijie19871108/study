02a5faaf5f11d7ef6cbc8b9328b8bbc9027fc95b
Alaska <nancylj23@126.com>
2016-01-17 23:13:32 +0800

    Update Constant.java
    测试git fetch

--- src/main/java/com/TYYJ/common/Constant.java
+++ src/main/java/com/TYYJ/common/Constant.java
@@ -2,6 +2,8 @@ package com.TYYJ.common;
 
 public class Constant {
 	
+	public static final String AppName = "TYYJ";
+	
 	public static final String HOST="127.0.0.1";
 	
 	public static final Integer PORT= 6379;
