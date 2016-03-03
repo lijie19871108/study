9501a792805c1e993bcafb5310e6c0a307bbc300
Alaska <nancylj23@126.com>
2016-03-03 16:35:29 +0800

    使用shiro，注册用户与登录用户成功

--- src/main/java/com/TYYJ/controller/LoginController.java
+++ src/main/java/com/TYYJ/controller/LoginController.java
@@ -3,22 +3,43 @@ package com.TYYJ.controller;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tyyj.db.service.UserService;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	UserService userServie;
+	
 	@RequestMapping(value="/login",method=RequestMethod.GET)
-	public @ResponseBody String login(){
+	public @ResponseBody String login(@RequestParam String userName, @RequestParam String password){
 		try {
-			SecurityUtils.getSubject().login(new UsernamePasswordToken("tom","000000"));
-			return "login successed!";
+			if(userServie.login(userName, password)){
+				return "login successed!";
+			}
+			else{
+				return "login error!";
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			return "login error:"+e.getMessage();
+		}
+	}
+	
+	@RequestMapping(value="/regist",method=RequestMethod.GET)
+	public @ResponseBody String regist(@RequestParam String userName, @RequestParam String password){
+		if(userServie.regist(userName, password)){
+			return "regist successed!";
+		}
+		else{
+			return "regist failed!";
 		}
 	}
 	
